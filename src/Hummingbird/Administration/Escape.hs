e728f3726f408219eb5a72894b414e99308632e9
sclausen <github@sebastian-clausen.de>
2017-03-17 18:42:08 +0100

    Fixed bad escaping and added hummingbird prompt icon.

--- src/Hummingbird/Administration/Escape.hs
+++ src/Hummingbird/Administration/Escape.hs
@@ -1,15 +1,18 @@
 module Hummingbird.Administration.Escape where
 
+import qualified Data.ByteString as BS
+import qualified Data.Text       as T
+
 banner :: String
 banner =
   "\ESC[0;31m" ++
   "|_     _ _  _ _ . _  _ |_ . _ _|\n" ++
   "| ||_|| | || | ||| |(_||_)|| (_|\n" ++
   "\ESC[0;33m---------------------"    ++
-  "\ESC[0;31m_|\ESC[0;33m---------\ESC[0m\STX"
+  "\ESC[0;31m_|\ESC[0;33m---------\ESC[0m"
 
 prompt :: String
-prompt = "\ESC[0;32m\STXhummingbird\ESC[0;35m>\ESC[0m\STX "
+prompt = "\ESC[0;32mhummingbird\ESC[0;35m \x1F54A \ESC[0m"
 
 leftPad :: Int -> Char -> String -> String
 leftPad i c s = replicate (i - length s) c ++ s
@@ -39,10 +42,7 @@ lightCyan  :: String -> String
 lightCyan = color "1;36"
 
 color :: String -> String -> String
-color c s = "\ESC[" ++ c ++ "m\STX" ++ s ++ "\ESC[0m\STX"
-
-dot   :: String
-dot    = "\x2022"
+color c s = "\ESC[" ++ c ++ "m" ++ s ++ "\ESC[0m"
 
 ago :: (Show a, Integral a) => a -> String
 ago uptime =
@@ -54,3 +54,9 @@ ago uptime =
     hours   = rem uptime (24*3600) `quot` 3600
     minutes = rem uptime 3600 `quot` 60
     seconds = rem uptime 60
+
+escapeText :: T.Text -> String
+escapeText = init . tail . show
+
+escapeByteString :: BS.ByteString -> String
+escapeByteString = init . tail . show
--- src/Hummingbird/Administration/Response.hs
+++ src/Hummingbird/Administration/Response.hs
@@ -2,23 +2,25 @@
 module Hummingbird.Administration.Response where
 
 import           Control.Monad
-import           Data.Maybe
-import           Data.List (intercalate)
-import qualified Data.Binary                       as B
 import           Control.Monad.IO.Class
+import qualified Data.Binary                           as B
 import           Data.Int
-import           Data.UUID                         (UUID)
-import           GHC.Generics                      (Generic)
+import           Data.List                             (intercalate)
+import           Data.Maybe
+import           Data.UUID                             (UUID)
+import           GHC.Generics                          (Generic)
+import           Network.MQTT.Broker.Authentication    (Principal (..),
+                                                        Quota (..))
+import           Network.MQTT.Broker.Session           (Connection,
+                                                        connectionCleanSession,
+                                                        connectionCreatedAt,
+                                                        connectionRemoteAddress,
+                                                        connectionSecure,
+                                                        connectionWebSocket)
+import qualified Network.MQTT.Broker.SessionStatistics as SS
+import           Network.MQTT.Message                  (ClientIdentifier (..),
+                                                        Username (..))
 import           System.Clock
-import           Network.MQTT.Broker.Authentication       (Principal (..), Quota (..))
-import           Network.MQTT.Message              (ClientIdentifier (..), Username (..))
-import           Network.MQTT.Broker.Session              (Connection,
-                                                    connectionCleanSession,
-                                                    connectionCreatedAt,
-                                                    connectionRemoteAddress,
-                                                    connectionSecure,
-                                                    connectionWebSocket)
-import qualified Network.MQTT.Broker.SessionStatistics    as SS
 
 import           Hummingbird.Administration.Escape
 
@@ -82,16 +84,16 @@ render p info@BrokerInfo {} = do
 --  format "Throughput         " "20,454/s 9,779/s 15,831/s"
   where
     format key value =
-      p $ cyan key ++ ": " ++ lightCyan value ++ "\ESC[0m\STX"
+      p $ cyan key ++ ": " ++ lightCyan value ++ "\ESC[0m"
 
 render p (Session s) = do
   now <- liftIO $ sec <$> getTime Realtime
   format "Alive since                    " $ ago $ now - sessionCreatedAt s
-  format "Client                         " $ showEscaped clientIdentifier
-  format "Principal                      " $ showEscaped (sessionPrincipalIdentifier s)
+  format "Client                         " $ escapeText clientIdentifier
+  format "Principal                      " $ show (sessionPrincipalIdentifier s)
   case principalUsername (sessionPrincipal s) of
     Nothing -> pure ()
-    Just (Username username) -> format "  Username                     " $ showEscaped username
+    Just (Username username) -> format "  Username                     " $ escapeText username
   p $ cyan "  Quota"
   format "    Max idle session TTL       " $ show (quotaSessionTTL $ principalQuota $ sessionPrincipal s)
   format "    Max inflight messages      " $ show (quotaMaxInflightMessages $ principalQuota $ sessionPrincipal s)
@@ -108,7 +110,7 @@ render p (Session s) = do
       format "  WebSocket                    " $ show (connectionWebSocket conn)
       case connectionRemoteAddress conn of
         Nothing   -> pure ()
-        Just addr -> format "  Remote Address               " $ showEscaped addr
+        Just addr -> format "  Remote Address               " $ escapeByteString addr
   p $ cyan "Statistics"
   format "  Publications  accepted       " $ show (SS.publicationsAccepted  $ sessionStatistics s)
   format "  Publications  dropped        " $ show (SS.publicationsDropped   $ sessionStatistics s)
@@ -119,7 +121,7 @@ render p (Session s) = do
   where
     ClientIdentifier clientIdentifier = sessionClientIdentifier s
     format key value =
-      p $ cyan key ++ ": " ++ lightCyan value ++ "\ESC[0m\STX"
+      p $ cyan key ++ ": " ++ lightCyan value ++ "\ESC[0m"
 
 
 render p (SessionList ss) = do
@@ -128,18 +130,15 @@ render p (SessionList ss) = do
     let x1 = leftPad 8 ' '  $ show $ sessionIdentifier session
     let x2 = status (sessionConnection session)
     let x3 = leftPad 12 ' ' $ ago $ now - sessionCreatedAt session
-    let x4 = leftPad 35 ' ' $ lightCyan $ showEscaped $ sessionPrincipalIdentifier session
+    let x4 = leftPad 35 ' ' $ lightCyan $ show $ sessionPrincipalIdentifier session
     let x5 = leftPad 24 ' ' $ showClientIdentifier (sessionClientIdentifier session)
-    let x6 = leftPad 12 ' ' $ fromMaybe "" $ showEscaped <$> (sessionConnection session >>= connectionRemoteAddress)
+    let x6 = leftPad 12 ' ' $ fromMaybe "" $ escapeByteString <$> (sessionConnection session >>= connectionRemoteAddress)
     p $ unwords [x1,x2,x3,x4,x5,x6]
   where
-    status Nothing                                   = lightRed   "DISCONNECTED"
-    status (Just conn) | connectionCleanSession conn = lightBlue  "CONNECTED   "
-                       | otherwise                   = lightGreen "CONNECTED+  "
-    showClientIdentifier (ClientIdentifier s) = take 24 $ showEscaped s
+    status Nothing                                   = lightRed   "IDLE"
+    status (Just conn) | connectionCleanSession conn = lightBlue  "TEMP"
+                       | otherwise                   = lightGreen "CONN"
+    showClientIdentifier (ClientIdentifier s) = take 24 $ escapeText s
 
 render p (SessionSubscriptions s) =
   p s
-
-showEscaped :: Show a => a -> String
-showEscaped = init . tail . show
