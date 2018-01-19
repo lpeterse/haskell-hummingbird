69f11e78b8113459dfd97fad5047bb50939232ad
Lars Petersen <info@lars-petersen.net>
2018-01-19 13:38:55 +0100

    Adapted to changes in haskell-mqtt.

--- src/Hummingbird/Transport.hs
+++ src/Hummingbird/Transport.hs
@@ -10,12 +10,16 @@ import           Data.Default
 import           Data.Int
 import qualified Data.Text                          as T
 import qualified Data.Text.Encoding                 as T
+import           Data.Typeable
 import           Data.Word
 import qualified Data.X509.CertificateStore         as X509
-import qualified Network.Stack.Server               as SS
 import qualified Network.TLS                        as TLS
 import qualified Network.TLS.Extra.Cipher           as TLS
 import qualified Network.WebSockets                 as WS
+import qualified Networking                         as SS
+import qualified Networking.Socket                  as SS
+import qualified Networking.TLS                     as SS
+import qualified Networking.WebSocket               as SS
 import qualified System.Log.Logger                  as LOG
 import qualified System.Socket                      as S
 import qualified System.Socket.Family.Inet          as S
@@ -25,6 +29,7 @@ import qualified System.Socket.Type.Stream          as S
 import qualified Network.MQTT.Broker                as Broker
 import           Network.MQTT.Broker.Authentication
 import qualified Network.MQTT.Broker.Server         as Server
+import           Network.MQTT.Message               (ClientIdentifier (..))
 
 run :: Authenticator auth => Broker.Broker auth -> [ Config ] -> IO ()
 run broker transportConfigs =
@@ -35,53 +40,29 @@ run broker transportConfigs =
 
 runTransport :: Authenticator auth => Broker.Broker auth -> Config -> IO ()
 runTransport broker transportConfig = case transportConfig of
-  SocketTransport {} -> do
-    cfg <- createSocketConfig transportConfig
-    let mqttConfig = Server.MqttServerConfig {
-        Server.mqttTransportConfig = cfg
-      } :: SS.ServerConfig (Server.MQTT (S.Socket S.Inet S.Stream S.Default))
-    runServerStack mqttConfig broker
-  TlsTransport {} -> do
-    cfg <- createSecureSocketConfig transportConfig
-    let mqttConfig = Server.MqttServerConfig {
-        Server.mqttTransportConfig = cfg
-      }
-    runServerStack mqttConfig broker
-  WebSocketTransport { wsTransport = tc@SocketTransport {}, wsFramePayloadSizeLimit = fpsl, wsMessageDataSizeLimit = mdsl } -> do
-    cfg <- createSocketConfig tc
-    let mqttConfig = Server.MqttServerConfig {
-      Server.mqttTransportConfig = SS.WebSocketServerConfig {
-        SS.wsTransportConfig = cfg
-      , SS.wsConnectionOptions = WS.defaultConnectionOptions {
-          WS.connectionFramePayloadSizeLimit = WS.SizeLimit fpsl
-        , WS.connectionMessageDataSizeLimit = WS.SizeLimit mdsl
-        }
-      }
-    }
-    runServerStack mqttConfig broker
-  WebSocketTransport { wsTransport = tc@TlsTransport {}, wsFramePayloadSizeLimit = fpsl, wsMessageDataSizeLimit = mdsl } -> do
-    cfg <- createSecureSocketConfig tc
-    let mqttConfig = Server.MqttServerConfig {
-      Server.mqttTransportConfig = SS.WebSocketServerConfig {
-        SS.wsTransportConfig = cfg
-      , SS.wsConnectionOptions = WS.defaultConnectionOptions {
-          WS.connectionFramePayloadSizeLimit = WS.SizeLimit fpsl
-        , WS.connectionMessageDataSizeLimit = WS.SizeLimit mdsl
-        }
-      }
-    }
-    runServerStack mqttConfig broker
+  SocketTransport {} ->
+    createSocketConfig transportConfig >>= runServerStack broker
+  TlsTransport {} ->
+    createSecureSocketConfig transportConfig >>= runServerStack broker
+  WebSocketTransport { wsTransport = t@SocketTransport {} } ->
+    createSocketConfig t >>= createWebSocketConfig transportConfig >>= runServerStack broker
+  WebSocketTransport { wsTransport = t@TlsTransport {} } ->
+    createSecureSocketConfig t >>= createWebSocketConfig transportConfig >>= runServerStack broker
   _ -> error "Server stack not implemented."
   where
-    createSocketConfig :: Config -> IO (SS.ServerConfig (S.Socket S.Inet S.Stream S.Default))
-    createSocketConfig (SocketTransport a p b) = do
+    createSocketConfig :: Config -> IO (SS.ServerConfig (S.Socket S.Inet S.Stream S.Default), SS.ServerHooks (S.Socket S.Inet S.Stream S.Default))
+    createSocketConfig (SocketTransport a p b l) = do
       (addrinfo:_) <- S.getAddressInfo (Just $ T.encodeUtf8 a) (Just $ T.encodeUtf8 $ T.pack $ show p) (mconcat [S.aiNumericHost, S.aiNumericService]) :: IO [S.AddressInfo S.Inet S.Stream S.Default]
-      pure SS.SocketServerConfig {
+      let cfg = SS.SocketServerConfig {
             SS.socketServerConfigBindAddress = S.socketAddress addrinfo
           , SS.socketServerConfigListenQueueSize = b
+          , SS.socketServerConfigConnectionLimit = l
         }
+      let hooks = SS.SocketServerHooks
+      pure (cfg, hooks)
     createSocketConfig _ = error "not a socket config"
-    createSecureSocketConfig :: Config -> IO (SS.ServerConfig (SS.TLS (S.Socket S.Inet S.Stream S.Default)))
+
+    createSecureSocketConfig :: Config -> IO (SS.ServerConfig (SS.Tls (S.Socket S.Inet S.Stream S.Default)), SS.ServerHooks (SS.Tls (S.Socket S.Inet S.Stream S.Default)))
     createSecureSocketConfig (TlsTransport tc cc ca crt key) = do
       mcs <- X509.readCertificateStore ca
       case mcs of
@@ -92,8 +73,8 @@ runTransport broker transportConfig = case transportConfig of
           case ecred of
             Left e -> error e
             Right credential -> do
-              cfg <- createSocketConfig tc
-              pure SS.TlsServerConfig {
+              (cfg, hooks) <- createSocketConfig tc
+              let cfg' = SS.TlsServerConfig {
                     SS.tlsTransportConfig = cfg
                   , SS.tlsServerParams    = def {
                       TLS.serverWantClientCert = cc
@@ -115,12 +96,34 @@ runTransport broker transportConfig = case transportConfig of
                       }
                     }
                   }
+              let hooks' = SS.TlsServerHooks {
+                  SS.tlsTransportHooks = hooks
+                , SS.tlsServerHooks = def
+                }
+              pure (cfg', hooks')
     createSecureSocketConfig _ = error "not a tls config"
 
-runServerStack :: (Authenticator auth, SS.StreamServerStack transport, Server.MqttServerTransportStack transport) => SS.ServerConfig (Server.MQTT transport) -> Broker.Broker auth -> IO ()
-runServerStack serverConfig broker =
-  SS.withServer serverConfig $ \server-> forever $ SS.withConnection server $ \connection info->
-    Server.serveConnection broker connection info
+    createWebSocketConfig :: Config -> (SS.ServerConfig a, SS.ServerHooks a) -> IO (SS.ServerConfig (SS.WebSocket a), SS.ServerHooks (SS.WebSocket a))
+    createWebSocketConfig (WebSocketTransport { wsFramePayloadSizeLimit = fpsl, wsMessageDataSizeLimit = mdsl }) (cfg, hooks) = do
+      let cfg' = SS.WebSocketServerConfig {
+          SS.wsTransportConfig   = cfg
+        , SS.wsConnectionOptions = WS.defaultConnectionOptions {
+            WS.connectionFramePayloadSizeLimit = WS.SizeLimit fpsl
+          , WS.connectionMessageDataSizeLimit = WS.SizeLimit mdsl
+          }
+        }
+      let hooks' = SS.WebSocketServerHooks {
+          SS.wsTransportHooks      = hooks
+        , SS.wsOnConnectionRequest = const $ pure $ Right $ WS.AcceptRequest (Just "mqtt") []
+        }
+      pure (cfg', hooks')
+    createWebSocketConfig _ _ = error "not a web socket config"
+
+runServerStack :: (Authenticator auth, SS.ServerStack transport, SS.StreamOriented transport, Server.MqttServerTransportStack transport) => Broker.Broker auth -> (SS.ServerConfig transport, SS.ServerHooks transport) -> IO ()
+runServerStack broker (config, hooks) =
+  SS.withServer config $ \server->
+    forever $ SS.serveForever server hooks $ \connection info->
+      Server.serveConnection broker connection info
 
 -------------------------------------------------------------------
 -- Configuration
@@ -128,9 +131,10 @@ runServerStack serverConfig broker =
 
 data Config
    = SocketTransport
-     { bindAddress   :: T.Text
-     , bindPort      :: Word16
-     , listenBacklog :: Int
+     { bindAddress     :: T.Text
+     , bindPort        :: Word16
+     , listenBacklog   :: Int
+     , connectionLimit :: Int
      }
    | WebSocketTransport
      { wsTransport             :: Config
@@ -157,7 +161,8 @@ instance FromJSON Config where
       "socket" -> SocketTransport
         <$> v .: "bindAddress"
         <*> v .: "bindPort"
-        <*> v .: "listenBacklog"
+        <*> v .:? "listenBacklog"         .!= 16
+        <*> v .:? "connectionLimit"       .!= 1024
       "tls" -> TlsTransport
         <$> v .: "transport"
         <*> v .: "wantClientCert"
@@ -166,3 +171,32 @@ instance FromJSON Config where
         <*> v .: "keyFilePath"
       _ -> fail "Expected 'socket', 'websocket' or 'tls'."
   parseJSON invalid = typeMismatch "Config" invalid
+
+instance (Typeable f, Typeable t, Typeable p, S.Family f, S.Protocol p, S.Type t, S.HasNameInfo f) => Server.MqttServerTransportStack (S.Socket f t p) where
+  getConnectionRequest (SS.SocketConnectionInfo addr) = do
+    remoteAddr <- S.hostName <$> S.getNameInfo addr (S.niNumericHost `mappend` S.niNumericService)
+    pure ConnectionRequest {
+        requestClientIdentifier = ClientIdentifier mempty
+      , requestSecure = False
+      , requestCleanSession = True
+      , requestCredentials = Nothing
+      , requestHttp = Nothing
+      , requestCertificateChain = Nothing
+      , requestRemoteAddress = Just remoteAddr
+      , requestWill = Nothing
+      }
+
+instance (SS.ServerStack a, Server.MqttServerTransportStack a) => Server.MqttServerTransportStack (SS.WebSocket a) where
+  getConnectionRequest (SS.WebSocketConnectionInfo tci rh) = do
+    req <- Server.getConnectionRequest tci
+    pure req {
+        requestHttp = Just (WS.requestPath rh, WS.requestHeaders rh)
+      }
+
+instance (SS.ServerStack a, Server.MqttServerTransportStack a) => Server.MqttServerTransportStack (SS.Tls a) where
+  getConnectionRequest (SS.TlsConnectionInfo tci mcc) = do
+    req <- Server.getConnectionRequest tci
+    pure req {
+        requestSecure = True
+      , requestCertificateChain = mcc
+      }
